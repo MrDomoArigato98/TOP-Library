02d8852d32d6f58c13827d2586637f015dd21512
MrDomoArigato98 <domdob98@gmail.com>
2025-02-21 12:57:45 +0000

    Adds functions to add books to library

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function Book(name, author, pages, isRead){
     this.pages = pages;
     this.isRead = isRead;
 }
+
+//Can make a prototype that prints whether it was Read or Not based on T / F of the isRead boolean.
+
+
+//Here we take some parameters, create your Book object, and add it to the list of books.
+function addBookToLibrary(){
+
+}
