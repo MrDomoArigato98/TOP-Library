7e6c95100a47a9f381c27bf904f944ae03d66119
MrDomoArigato98 <domdob98@gmail.com>
2025-02-22 09:40:33 +0000

    Adds books to library

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let myLibrary = ["nineteenEightyfour"];
 
 function Book(name, author, pages, isRead){
     this.name = name;
@@ -9,6 +9,8 @@ function Book(name, author, pages, isRead){
 
 //Can make a prototype that prints whether it was Read or Not based on T / F of the isRead boolean.
 
+let nineteenEightyfour = new Book("1984","George Orwell", 328, true);
+let the48LawsofPower = new Book("The 48 Laws of Power","Robert Greene", 480, true);
 
 //Here we take some parameters, create your Book object, and add it to the list of books.
 function addBookToLibrary(){
