4b037760b2efec4f398387fc2b2109a962ffa46e
MrDomoArigato98 <domdob98@gmail.com>
2025-02-22 12:00:35 +0000

    Adds modal to HTML and button functions to JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let myLibrary = ["nineteenEightyfour"];
+const newBookButton = document.getElementById("addNewBook");
+const dialog = document.getElementById("dialog")
 
-function Book(name, author, pages, isRead){
+let myLibrary = [];
+
+function Book(name, author, pages, isRead) {
     this.name = name;
     this.author = author;
     this.pages = pages;
@@ -9,10 +12,26 @@ function Book(name, author, pages, isRead){
 
 //Can make a prototype that prints whether it was Read or Not based on T / F of the isRead boolean.
 
-let nineteenEightyfour = new Book("1984","George Orwell", 328, true);
-let the48LawsofPower = new Book("The 48 Laws of Power","Robert Greene", 480, true);
 
 //Here we take some parameters, create your Book object, and add it to the list of books.
-function addBookToLibrary(){
+function addBookToLibrary(name, author, pages, isRead) {
+    let book = new Book(name, author, pages, isRead);
+    myLibrary.push(book)
+}
+
+
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("The 48 Laws of Power", "Robert Greene", 480, true);
+
+console.log(myLibrary[0])
+console.log(myLibrary[1])
+function displayBook() {
 
 }
+
+
+newBookButton.addEventListener("click", () => {
+    console.log("test");
+
+    dialog.showModal();
+});
