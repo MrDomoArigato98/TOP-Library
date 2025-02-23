cf01b4476ecf56d497f2f05261338361ce3d9d3e
MrDomoArigato98 <domdob98@gmail.com>
2025-02-23 14:59:23 +0000

    No idea

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//Button for new books top right
 const newBookButton = document.getElementById("add-new-book");
 
+//Gets the Dialog element of the page using ID
 const dialog = document.getElementById("dialog")
-
+//These are all the inputs from the dialog
 const dialogInputArray = document.querySelectorAll("input");
-
+const userForm = document.getElementById("user-form")
 const template = document.getElementById("template");
 
 let myLibrary = [];
@@ -75,27 +77,48 @@ function setUniqueId(bookCard){
 }
 
 
-
-console.log(newBookButton)
-
 newBookButton.addEventListener("click", function(){
     dialog.showModal();
     dialog.focus();
 })
 
-
-console.log(dialogInputArray)
-
 dialogInputArray.forEach(element => {
     element.addEventListener("click", function(e){
-        if(element.id=="submit"){
+        if(element.id=="submit-btn"){
             console.log("Submit");
+            
+            
+    getDialogInput()
 
         }
-        if(element.id=="cancel"){
+        if(element.id=="cancel-btn"){
             console.log("Cancel")
             e.preventDefault();
             dialog.close();
         }
     })
 });
+
+console.log(dialog)
+console.log(dialogInputArray)
+
+function getDialogInput(){
+    let authorInput = document.getElementById('author-input').value;
+    let titleInput = document.getElementById('title-input').value;
+    let pagesInput = document.getElementById('pages-input').value;
+    const selectedOption = document.querySelector('input[name="read-toggle"]:checked');
+    let readInput = selectedOption.id
+    
+    validateForm(authorInput,titleInput,pagesInput,readInput)
+
+    console.log(authorInput)
+    console.log(titleInput)
+    console.log(pagesInput)
+    console.log(readInput)
+}
+
+function validateForm(authorInput,titleInput,pagesInput,readInput){
+    if(authorInput==""){
+        console.log("No author")
+    }
+}
