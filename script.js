2225fdb47ff97fea7aa0083ecf26865ad2b32e87
MrDomoArigato98 <domdob98@gmail.com>
2025-02-24 09:33:21 +0000

    Adds prototype to toggle read status

--- script.js
+++ script.js
@@ -16,9 +16,13 @@ function Book(title, author, pages, isRead) {
     this.pages = pages;
     this.isRead = isRead;
 }
-
-//Can make a prototype that prints whether it was Read or Not based on T / F of the isRead boolean.
-
+Book.prototype.toggleRead = function(){
+    if (this.isRead=="read"){
+        this.isRead ="not-read"
+    }else{
+        this.isRead=="read"
+    }
+}
 
 //Here we take some parameters, create your Book object, and add it to the list of books.
 function addBookToLibrary(title, author, pages, isRead) {
