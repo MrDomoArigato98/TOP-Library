b2d4acc67d05b46ecd0791e66e64e13cc97f09ea
MrDomoArigato98 <domdob98@gmail.com>
2025-02-24 10:08:30 +0000

    Uses classes in ES6

--- script.js
+++ script.js
@@ -10,33 +10,27 @@ const template = document.getElementById("template");
 
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-}
-Book.prototype.toggleRead = function () {
-    if (this.isRead == "read") {
-        console.log("not-read")
-        this.isRead = "not-read"
-    } else {
-        this.isRead = "read"
-        console.log("read")
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+
+    toggleRead() {
+        this.isRead = this.isRead === "read" ? "not-read" : "read";
     }
 }
 
-//Here we take some parameters, create your Book object, and add it to the list of books.
 function addBookToLibrary(title, author, pages, isRead) {
-    let book = new Book(title, author, pages, isRead);
-    myLibrary.push(book)
+    const book = new Book(title, author, pages, isRead);
+    myLibrary.push(book);
     displayBook(book);
 }
 
-
 addBookToLibrary("1984", "George Orwell", 328, "read");
 addBookToLibrary("The 48 Laws of Power", "Robert Greene", 480, "not-read");
-
 
 function displayBook(book) {
     console.log(book)
@@ -50,18 +44,15 @@ function displayBook(book) {
 
     document.getElementById("library").appendChild(newBookCard);
 
-    toggleDisplayReadStatus(newBookCard,book)
-    bookButtonsListeners(newBookCard,book);
-    setUniqueId(newBookCard)
+    toggleDisplayReadStatus(newBookCard, book);
+    bookButtonsListeners(newBookCard, book);
+    setUniqueId(newBookCard);
 }
 
-function toggleDisplayReadStatus(bookCard,book){
-    if (book.isRead == "read") {
-        bookCard.querySelector("input").checked = true;
-    } else {
-        bookCard.querySelector("input").checked = false;
-    }
+function toggleDisplayReadStatus(bookCard, book) {
+    bookCard.querySelector("input").checked = book.isRead === "read";
 }
+
 function bookButtonsListeners(bookCard, book) {
     bookIndex = bookCard.getAttribute("bookindex");
     console.log(bookIndex);
@@ -72,24 +63,14 @@ function bookButtonsListeners(bookCard, book) {
     bookButtons.forEach(button => {
         button.addEventListener("click", function () {
 
-            if (button.getAttribute("class") == "read-button") {
-                console.log("Read-button")
+            if (button.classList.contains("read-button")) {
                 book.toggleRead();
-                toggleDisplayReadStatus(bookCard,book)
+                toggleDisplayReadStatus(bookCard, book);
             }
 
-            if (button.getAttribute("class") == "borrow-button") {
-                console.log("borrow")
-                if (myLibrary.length == 1) {
-                    myLibrary.length = 0;
-                }
-
-                bookCard.remove(); // Removes only this book card from DOM
-
-                bookIndex = bookCard.getAttribute("bookindex");
-                console.log(bookIndex);
-
-                myLibrary.splice(bookIndex, 1) //Remove from list of books
+            if (button.classList.contains("borrow-button")) {
+                bookCard.remove();
+                myLibrary.splice(bookCard.getAttribute("bookindex"), 1);
             }
 
         })
@@ -99,7 +80,6 @@ function bookButtonsListeners(bookCard, book) {
 function setUniqueId(bookCard) {
 
 }
-
 
 newBookButton.addEventListener("click", function () {
     resetDialogInput();
@@ -125,15 +105,14 @@ console.log(dialog)
 console.log(dialogInputArray)
 
 function getDialogInput() {
-    let authorInput = document.getElementById('author-input').value;
-    let titleInput = document.getElementById('title-input').value;
-    let pagesInput = document.getElementById('pages-input').value;
-    const selectedOption = document.querySelector('input[name="read-toggle"]:checked');
-    let readInput = selectedOption.id
+    const authorInput = document.getElementById('author-input').value;
+    const titleInput = document.getElementById('title-input').value;
+    const pagesInput = document.getElementById('pages-input').value;
+    const readInput = document.querySelector('input[name="read-toggle"]:checked').id;
 
-    validateForm(authorInput, titleInput, pagesInput, readInput)
-
+    validateForm(authorInput, titleInput, pagesInput, readInput);
 }
+
 function resetDialogInput() {
     let authorInput = document.getElementById('author-input').value = ""
     let titleInput = document.getElementById('title-input').value = ""
@@ -141,9 +120,9 @@ function resetDialogInput() {
 }
 
 function validateForm(authorInput, titleInput, pagesInput, readInput) {
-    if (authorInput === "" || titleInput === "" || pagesInput === "") {
-        console.log("Missing field")
+    if (authorInput && titleInput && pagesInput) {
+        addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
     } else {
-        addBookToLibrary(titleInput, authorInput, pagesInput, readInput)
+        console.log("Missing field");
     }
 }
